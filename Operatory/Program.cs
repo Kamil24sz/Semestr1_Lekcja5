5384c1c2e6531cddfdb1a65e3201bc4a17474bff
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-10-18 20:07:17 +0200

    Add project files.

---
+++ Operatory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            decimal budżet = 40;
+            decimal kosztPizzy = 40;
+
+            bool czyWystarczy = false;
+
+            czyWystarczy = kosztPizzy <= budżet;
+
+            Console.WriteLine("Budżet: " + budżet);
+            Console.WriteLine("Koszt Pizzy: " + kosztPizzy);
+            Console.WriteLine("Czy wystraczy: " + czyWystarczy);
+
+            Console.WriteLine();
+            //tutaj wybrany rodzaj pizzy
+            string rodzajPizzy = "mięsna";
+            Console.WriteLine("Obecny rodzaj pizzy: " + rodzajPizzy);
+
+            //pizza jaką powinniśmy zamówić
+            string oczekiwanyRodzaj = "pieczarkowa";
+            Console.WriteLine("Rodzina chce taki rodzaj pizzy: " + oczekiwanyRodzaj);
+
+
+            bool czyPizzaBędzieSmakowała = rodzajPizzy != oczekiwanyRodzaj;
+            Console.WriteLine("Czy pizza będzie samakowała: " + czyPizzaBędzieSmakowała);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rzutowaniee/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rzutowaniee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rzutowanie - konswersja
+
+            //to było to jest parsowanie
+            int liczba = int.Parse(Console.ReadLine());
+            //wypisywanie liczbu urzytkownia
+            Console.WriteLine("Liczba oryginalna (int) "+liczba);
+
+            double liczba2 = (double)liczba + 0.5;
+            //wypisywanie przekonwertowniej liczby (doble)
+            Console.WriteLine("Liczba przekonwertowna do double "+liczba2);
+
+            //powrót do inta
+            int suma = (int)liczba2;
+            Console.WriteLine("Liczba przekonwertowna z powrotem do int " + suma);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
